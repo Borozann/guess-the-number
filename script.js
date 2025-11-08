d8f888cf4873d2179c8480f6059ae48d3cb52a30
Borozann <gabriel.borozan83@gmail.com>
2025-11-08 15:27:21 -0300

    Adding JS script

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+const displayMessage = function (newMessage) {
+  document.querySelector('.message').textContent = newMessage;
+};
+
+let highscore = localStorage['highScoreCache'];
+let highscoreElement = document.querySelector('.highscore');
+
+if (highscore) {
+  highscoreElement.textContent = highscore;
+} else {
+  highscoreElement.textContent = 0;
+}
+
+let score = 0;
+let alreadyWin = false;
+
+const correctNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //Quebra se guess == undefined
+  if (!guess) {
+    displayMessage('Empty Field');
+    return;
+  }
+
+  score++;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = guess;
+
+  if (highscore > score) {
+    highscoreElement.textContent = score;
+    localStorage['highScoreCache'] = highscoreElement.textContent;
+  }
+
+  //Quebra se ganhar
+  if (guess === correctNumber && !alreadyWin) {
+    alreadyWin = true;
+    displayMessage('Correct Number!!!');
+    document.querySelector('body').style.backgroundColor = '#8ae28a';
+
+    document.querySelector('.number').style.width = '30rem';
+    return;
+  }
+
+  displayMessage(
+    guess > correctNumber
+      ? 'Guess a lower number...'
+      : 'Guess a bigger number...'
+  );
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  window.location.reload();
+});
